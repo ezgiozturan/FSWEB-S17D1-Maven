7a6641a19eaf9bb89aff6b2b9caa8cfa9303369d
ezgiozturan <ezgiozturan05@gmail.com>
2024-08-20 00:30:33 +0300

    S17g1

---
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -0,0 +1,57 @@
+package com.workintech.fswebs17d1.controller;
+
+import com.workintech.fswebs17d1.entity.Animal;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/workintech/animal")
+public class AnimalController {
+
+    private Map<Integer, Animal> animals;
+
+    @PostConstruct
+    public void loadAll() {
+        System.out.println("çalışıyor");
+        this.animals = new HashMap<>();
+        this.animals.put( 1,new Animal(1,"maymun"));
+    }
+
+    @GetMapping
+    public List<Animal> getAnimals() {
+        return new ArrayList<>(animals.values());
+    }
+
+    @GetMapping("/animal/{id}")
+    public Animal getAnimalById (@PathVariable("id") int id) {
+      if(id<0) {
+          System.out.println("Id 0'dan küçük olamaz"+id);
+          return null;
+      }
+      return this.animals.get(id);
+    }
+
+    @PostMapping
+    public void addAnimal (@RequestBody Animal animal) {
+      this.animals.put(animal.getId(),animal);
+    }
+
+    @PutMapping("{id}")
+    public Animal updateAnimal(@PathVariable("id") int id , @RequestBody Animal newAnimal) {
+        this.animals.replace(id,newAnimal);
+        return this.animals.get(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteAnimal(@PathVariable("id") int id) {
+        this.animals.remove(id);
+    }
+
+
+
+}
---
+++ src/main/java/com/workintech/fswebs17d1/entity/integer.java
@@ -0,0 +1,4 @@
+package com.workintech.fswebs17d1.entity;
+
+public class integer {
+}
